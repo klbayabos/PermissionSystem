f296e4442e277fa621fc0bee3ecab7a891399bec
Jon Aruta <jon.aruta@gmail.com>
2016-06-20 16:59:28 +0800

    something

--- 195project/resources/views/manage_acc.blade.php
+++ 195project/resources/views/manage_acc.blade.php
@@ -34,13 +34,29 @@
 			.center{
 				text-align:center;
 			}
+			table{
+				table-layout: fixed;
+				border: 1px solid #dddddd;
+				border-collapse:collapse;
+				width:60%;
+				margin-bottom: 30px;
+			}
+			td, th{
+				border-bottom: 1px solid #dddddd;
+				width:25%;
+				text-align:center;
+			}
+			th{
+				background-color:#dddddd;
+			}
         </style>
     </head>
     <body>
 		*manage_acc.blade.php*
 		<center>
+		{{--
 		<table>
-		<tr><td colspan=2 valign="top" class="center" style="padding-bottom:30px;padding-top:20px"><h1>Manage Account</h1><td></tr>
+		<tr><td colspan=2 valign="top" class="center" style="padding-bottom:30px;padding-top:20px"><h1>Manage Accounts</h1><td></tr>
 		<form method="post">
 			<tr><td class="left">Email:</td> <td class="right">
 				<select class="selectpicker" name="name">
@@ -58,9 +74,13 @@
 				  <option>Admin</option>
 				</select>
 			</td></tr>
-			
 			<tr><td class="left"></td><td class="right"><input type="submit"></td></tr>
 		</form>
+		</table>
+		--}}
+		<table>
+			<tr><th style="text-align:center;">Name</th><th style="text-align:center;">Email</th><th style="text-align:center;">Type</th></tr>
+			<tr></tr>
 		</table>
 		</center>
     </body>
