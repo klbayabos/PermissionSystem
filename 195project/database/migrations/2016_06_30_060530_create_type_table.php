96a5f3f221176498f67d0a5d20e6288a238e5805
Jon Aruta <jon.aruta@gmail.com>
2016-06-30 14:18:20 +0800

    team & type migrations

---
+++ 195project/database/migrations/2016_06_30_060530_create_type_table.php
@@ -0,0 +1,40 @@
+<?php<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTypeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+		Schema::create('type', function(Blueprint $table){
+			$table->increments('type_id');
+			$table->string('name');
+		});
+		Schema::table('type', function($table){
+			DB::statement("INSERT INTO `type` (`name`) VALUES
+							('Officer in Charge'),
+							('Admin'),
+							('Approver'),
+							('Supervisor'),
+							('Human Resources'),
+							('Employee')
+			");
+		});
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
