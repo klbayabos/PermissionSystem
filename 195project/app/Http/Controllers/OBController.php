d654b2a84b0717e1be4a075f40986375d250dedc
Jon Aruta <jon.aruta@gmail.com>
2016-07-01 15:38:58 +0800

    storing requests in database

--- 195project/app/Http/Controllers/OBController.php
+++ 195project/app/Http/Controllers/OBController.php
@@ -5,8 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use DB;
 use Session;
+use App\Process;
+use App\RequestApplication;
+use App\OBRequestData;
 use App\Http\Requests;
 use Illuminate\Support\Facades\Redirect;
+use Carbon\Carbon;
 
 class OBController extends Controller
 {
@@ -33,8 +37,43 @@ class OBController extends Controller
     }
 	
 	// when submitting your ob request form
-	public function get_OBrequest()
+	public function get_OBrequest(Request $request)
     {
+		$time=Carbon::now();
+		$time=$time->toAtomString();
+		$status = DB::table('state_type')
+					->where('name','submitted')
+					->first();
+		$input = $request->all();
+		$process = new Process;
+		$process->name = \Auth::user()->id.'_'.$time;
+		$saved = $process->save();
+		if(!$saved){
+			App::abort(500, 'Error');
+		}
+		$req = new RequestApplication;
+		$req->id = \Auth::user()->id;
+		$req->type = "OB";
+		$req->process_id = $process->process_id;
+		$req->team_id = \Auth::user()->team_id;
+		$req->starting_date = $input['fromdate'];
+		$req->end_date = $input['todate'];
+		$req->starting_time = $input['fromtime'];
+		$req->end_time = $input['totime'];
+		$req->request_purpose = $input['purpose'];
+		$req->status = $status->state_type_id;
+		$saved = $req->save();
+		if(!$saved){
+			App::abort(500, 'Error');
+		}
+		$obdata = new OBRequestData;
+		$obdata->request_id = $req->request_id;
+		$obdata->to = $input['to'];
+		$obdata->from = $input['from'];
+		$saved = $obdata->save();
+		if(!$saved){
+			App::abort(500, 'Error');
+		}
 		Session::flash('emp_ob_msg', 'Your OB request has been submitted!');
 		return Redirect::to('/officialbusiness');			
     }
