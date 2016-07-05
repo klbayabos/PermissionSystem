2eeb67aa90332ac9dc26065ced37ca12e7c14361
Kathleen Bayabos <kathleen.klb@gmail.com>
2016-07-05 09:40:39 +0800

    added functions in controller

--- 195project/app/Http/Controllers/OBController.php
+++ 195project/app/Http/Controllers/OBController.php
@@ -13,16 +13,14 @@ use App\Http\Requests;
 use Illuminate\Support\Facades\Redirect;
 use Carbon\Carbon;
 
-class OBController extends Controller
-{
-	public function __construct()
-    {
+class OBController extends Controller{
+	
+	public function __construct(){
         $this->middleware('auth');
     }
 	
 	// display view of obform 
-	public function view_obform()
-    {
+	public function view_obform(){
 		$user = DB::table('users')
 					->leftJoin('team', 'users.team_id', '=', 'team.team_id')
 					->select('team.name as team', 'users.*')
@@ -32,25 +30,25 @@ class OBController extends Controller
     }
 	
 	// when deleting your ob request
-	public function del_ob()
-    {
+	public function del_ob(){
 		return Redirect::to('/officialbusiness');		// view your ob requests
     }
-	//view the details of an OB request
-	public function view_OB_details($request_id = NULL)
-	{
+	
+	// get details of ob request from DB
+	public function get_obdetails_DB($request_id){
 		$ob = DB::table('request')
-					->leftJoin('users', 'request.id', '=', 'users.id')
-					->leftJoin('state','state.state_id', '=', 'request.status')
-					->leftJoin('state_type','state_type.state_type_id', '=', 'state.state_type_id')
-					->leftJoin('ob_request_data', 'request.request_id', '=', 'ob_request_data.request_id')
-					->select('request.*','users.name','ob_request_data.to','ob_request_data.from','state_type.name as state')
-					->where('request.request_id', $request_id)
-					->first();
-		$ob_notes = DB::table('request_note')
-					->where('request_id', $request_id)
-					->get();
+				->leftJoin('users', 'request.id', '=', 'users.id')
+				->leftJoin('state','state.state_id', '=', 'request.status')
+				->leftJoin('state_type','state_type.state_type_id', '=', 'state.state_type_id')
+				->leftJoin('ob_request_data', 'request.request_id', '=', 'ob_request_data.request_id')
+				->select('request.*','users.name','ob_request_data.to','ob_request_data.from','state_type.name as state')
+				->where('request.request_id', $request_id)
+				->first();
 				
+		$ob_notes = DB::table('request_note')
+				->where('request_id', $request_id)
+				->get();
+			
 		// get team leader
 		$tl = DB::table('team')
 				->join('users', 'team.team_id', '=', 'users.team_id')
@@ -65,18 +63,39 @@ class OBController extends Controller
 				->where('users.type_id', 5)
 				->first();
 				
+		$array_ans = array($ob, $ob_notes, $tl, $sv);
+		return $array_ans;
+	}
+	
+	//view the details of an OB request
+	public function view_OB_details($request_id = NULL){
+		$val = $this->get_obdetails_DB($request_id);
+		$ob = $val[0];
+		$ob_notes = $val[1];
+		$tl = $val[2];
+		$sv = $val[3];
 		return view('my_ob', ['ob' => $ob, 'obnotes' => $ob_notes, 'tl' => $tl, 'sv' => $sv]);
 	}
+	
+	//view the details of an OB request for approval
+	public function view_OB_apdetails($request_id = NULL){
+		$val = $this->get_obdetails_DB($request_id);
+		$ob = $val[0];
+		$ob_notes = $val[1];
+		$tl = $val[2];
+		$sv = $val[3];
+		return view('ob_approval_details', ['ob' => $ob, 'obnotes' => $ob_notes, 'tl' => $tl, 'sv' => $sv]);
+	}
+	
 	// view user's ob requests
-	public function view_your_OB()
-	{
+	public function view_your_OB(){
 		$obs = DB::select("SELECT * FROM (SELECT team_id, name AS team FROM team) AS der1 NATURAL JOIN (SELECT * FROM request WHERE type='OB') as der2 NATURAL JOIN ob_request_data");
 		$count = count($obs);
 		return view('emp_ob', ['obs' => $obs, 'count' => $count]);
 	}
+	
 	// when submitting your ob request form
-	public function get_OBrequest(Request $request)
-    {
+	public function get_OBrequest(Request $request){
 		$time=Carbon::now();
 		$time=$time->toAtomString();
 		$status = DB::table('state_type')
@@ -125,8 +144,7 @@ class OBController extends Controller
     }
 	
 	// when approving or denying an ob request
-	public function ob_approval_action(Request $request)
-    {
+	public function ob_approval_action(Request $request){
 		$input = $request->all();
 		
 		if ($input['action'] == "Approve"){
@@ -141,45 +159,12 @@ class OBController extends Controller
     }
 	
 	// sorting ob request by name
-	public function sort_ob_name()
-    {
+	public function sort_ob_name(){
 		return Redirect::to('/aplist');			
     }
 	
 	// sorting ob request by team
-	public function sort_ob_team()
-    {
+	public function sort_ob_team(){
 		return Redirect::to('/aplist');			
     }
-	//view the details of an OB request for approval
-	public function view_OB_apdetails($request_id = NULL)
-	{
-		$ob = DB::table('request')
-					->leftJoin('users', 'request.id', '=', 'users.id')
-					->leftJoin('state','state.state_id', '=', 'request.status')
-					->leftJoin('state_type','state_type.state_type_id', '=', 'state.state_type_id')
-					->leftJoin('ob_request_data', 'request.request_id', '=', 'ob_request_data.request_id')
-					->select('request.*','users.name','ob_request_data.to','ob_request_data.from','state_type.name as state')
-					->where('request.request_id', $request_id)
-					->first();
-		$ob_notes = DB::table('request_note')
-					->where('request_id', $request_id)
-					->get();
-				
-		// get team leader
-		$tl = DB::table('team')
-				->join('users', 'team.team_id', '=', 'users.team_id')
-				->where('users.team_id', \Auth::user()->team_id)
-				->where('users.type_id', 7)
-				->first();
-		
-		// get supervisor
-		$sv = DB::table('team')
-				->join('users', 'team.team_id', '=', 'users.team_id')
-				->where('users.team_id', \Auth::user()->team_id)
-				->where('users.type_id', 5)
-				->first();
-				
-		return view('ob_approval_details', ['ob' => $ob, 'obnotes' => $ob_notes, 'tl' => $tl, 'sv' => $sv]);
-	}
 }
