5da8ad429fa16e34c5e4bbea629a8772c60b52ae
Jon Aruta <jon.aruta@gmail.com>
2016-07-06 12:01:25 +0800

    approval list layout..

--- 195project/resources/views/approval_list.blade.php
+++ 195project/resources/views/approval_list.blade.php
@@ -11,6 +11,7 @@
 		<!-- do not delete: for pop up stuff -->
 		<script src="{{ URL::asset('https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js') }}"></script>
 		<script src="{{ URL::asset('http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js') }}"></script>
+		<!-- <script type="text/javascript" src="{{ URL::asset('bower_components/bootstrap-responsive-tabs/js/responsive-tabs.js') }}"></script> -->
         <style>
            
 			body {
@@ -36,17 +37,20 @@
 				border-style: inset;
 				border-width: 1px;
 			}
-			
 			/* table */
 			
-			.container{
-				max-width:700px;
-			}
 			table{
 				table-layout: fixed;
 				border: 1px solid #dddddd;
 				letter-spacing: 1px;
 				width:700px;
+			}
+			#table a{
+				display:block;
+				text-decoration:none;
+			}
+			#table a:hover{
+				background-color:#cccccc;
 			}
 			td, th{
 				text-align: center;
@@ -59,7 +63,7 @@
 			h4{
 				text-align: left !important;
 			}
-			@media screen and (max-width:700px){
+			@media screen and (max-width:770px){
 				tr:nth-child(odd) {background: #DDD}
 				tr:nth-child(even) {background: #FFF}
 				th{
@@ -89,7 +93,7 @@
 				}
 				td:nth-of-type(3):before {
 					font-weight:bold;
-					content: "Date Requested: ";
+					content: "Dates Requested: ";
 				}
 				td:nth-of-type(4):before {
 					font-weight:bold;
@@ -123,63 +127,66 @@
 		<center>
 		<h2 style="margin-top:20px;">Requests for Approval</h1>
 		<br><br><br>
-	
-		@if($obs != null)
-			<h4>Official Business Requests</h4>
-			<!-- Sorting -->
-			<p style="text-align: left; padding-bottom:5px;"><label> <a href="/obrequest_sortname">Sort by name </a></label> | <label> <a href="/obrequest_sortteam"> Sort by team  </a></label></p>
-			<div class="container" style="padding:0;">
-				<table>
-					<tr><th style="text-align: center;">Name</th><th style="text-align: center;">Team</th><th style="text-align: center;">OB Date</th><th style="text-align: center;">Date Submitted</th><th style="text-align: center;">Status</th></tr>
-					@foreach($obs as $obs)
-						<tr><td>{{ $obs->name }}</td><td>{{ $obs->team }}</td><td>{{ date("m/d/Y", strtotime($obs->starting_date)) }} - {{ date("m/d/Y", strtotime($obs->end_date)) }}</td><td>{{ date("m/d/Y", strtotime($obs->created_at)) }}</td><td>{{ $obs->state }}<br><a href="/ob_apdetails/{{ $obs->request_id }}">View Details</a></td></tr>
-					@endforeach
-				</table>
-			</div>
+				<div class="container">
+					<ul class="nav nav-tabs">
+						<li class="active"><a data-toggle="tab" href="#ob">Official Business</a></li>
+						<li><a data-toggle="tab" href="#ot">Overtime</a></li>
+						<li><a data-toggle="tab" href="#on">Overnight</a></li>
+					</ul>
+
+					<div class="tab-content">
+						@if($obs != null)
+						<div id="ob" class="tab-pane fade in active">
+							<h4>Official Business Requests</h4>
+							<!-- Sorting -->
+							<p style="text-align: left; padding-bottom:5px;"><label> <a href="/obrequest_sortname">Sort by name </a></label> | <label> <a href="/obrequest_sortteam"> Sort by team  </a></label></p>
+							<table id="table">
+								<tr><th style="text-align: center;">Name</th><th style="text-align: center;">Team</th><th style="text-align: center;">Dates Requested</th><th style="text-align: center;">Date Submitted</th><th style="text-align: center;">Status</th></tr>
+							@foreach($obs as $obs)
+								<tr><td>{{ $obs->name }}</td><td>{{ $obs->team }}</td><td>{{ date("m/d/Y", strtotime($obs->starting_date)) }} - {{ date("m/d/Y", strtotime($obs->end_date)) }}</td><td>{{ date("m/d/Y", strtotime($obs->created_at)) }}</td><td>{{ $obs->state }}<br><a href="/ob_apdetails/{{ $obs->request_id }}">View Details</a></td></tr>
+							@endforeach
+							</table>
+						</div>
+						@endif
+						@if($ots != null)
+						<div id="ot" class="tab-pane fade">
+							<h4>Overtime Requests</h4>
+							<!-- Sorting -->
+							<p style="text-align: left; padding-bottom:5px;"><label> <a href="/obrequest_sortname">Sort by name </a></label> | <label> <a href="/obrequest_sortteam"> Sort by team  </a></label></p>
+							<table id="table">
+								<tr><th style="text-align: center;">Name</th><th style="text-align: center;">Team</th><th style="text-align: center;">Dates Requested</th><th style="text-align: center;">Date Submitted</th><th style="text-align: center;">Status</th></tr>
+							@foreach($ots as $ots)
+								<tr><td>{{ $ots->name }}</td><td>{{ $ots->team }}</td><td>{{ date("m/d/Y", strtotime($ots->starting_date)) }} - {{ date("m/d/Y", strtotime($ots->end_date)) }}</td><td>{{ date("m/d/Y", strtotime($ots->created_at)) }}</td><td>{{ $ots->state }}<br><a href="/ot_apdetails/{{ $ots->request_id }}">View Details </a></td></tr>
+							@endforeach
+							</table>
+						</div>
+						@endif
+						@if($ons != null)
+						<div id="on" class="tab-pane fade">
+							<h4>Overnight Requests</h4>
+							<!-- Sorting -->
+							<p style="text-align: left; padding-bottom:5px;"><label> <a href="/obrequest_sortname">Sort by name </a></label> | <label> <a href="/obrequest_sortteam"> Sort by team  </a></label></p>
+							<table id="table">
+								<tr><th style="text-align: center;">Name</th><th style="text-align: center;">Team</th><th style="text-align: center;">Dates Requested</th><th style="text-align: center;">Date Submitted</th><th style="text-align: center;">Status</th></tr>
+							@foreach($ons as $ons)
+								<tr><td>{{ $ons->name }}</td><td>{{ $ons->team }}</td><td>{{ date("m/d/Y", strtotime($ons->starting_date)) }} - {{ date("m/d/Y", strtotime($ons->end_date)) }}</td><td>{{ date("m/d/Y", strtotime($ons->created_at)) }}</td><td>{{ $ons->state }}<br><a href="/on_apdetails/{{ $ons->request_id }}">View Details</a></td></tr>
+							@endforeach
+							</table>
+						</div>
+						@endif
+					</div>
+				</div>
 			<br><br>
-		@endif
-		
-		@if($ots != null)
-			<h4>Overtime Requests</h4>
-			<!-- Sorting -->
-			<p style="text-align: left; padding-bottom:5px;"><label> <a href="/otrequest_sortname">Sort by name </a></label> | <label> <a href="/otrequest_sortteam"> Sort by team  </a></label></p>
-			
-			<div class="container" style="padding:0;">
-				<table>
-					<tr><th style="text-align: center;">Name</th><th style="text-align: center;">Team</th><th style="text-align: center;">Overtime Date</th><th style="text-align: center;">Date Submitted</th><th style="text-align: center;">Status</th></tr>
-					@foreach($ots as $ots)
-						<tr><td>{{ $ots->name }}</td><td>{{ $ots->team }}</td><td>{{ date("m/d/Y", strtotime($ots->starting_date)) }} - {{ date("m/d/Y", strtotime($ots->end_date)) }}</td><td>{{ date("m/d/Y", strtotime($ots->created_at)) }}</td><td>{{ $ots->state }}<br><a href="/ot_apdetails/{{ $ots->request_id }}">View Details </a></td></tr>
-					@endforeach
-				</table>
-			</div>
-		<br><br>
-		@endif
-		
-		@if($ons != null)
-			<h4>Overnight Requests</h4>
-			<!-- Sorting -->
-			<p style="text-align: left; padding-bottom:5px;"><label> <a href="/onrequest_sortname">Sort by name </a></label> | <label> <a href="/onrequest_sortteam"> Sort by team  </a></label></p>
-			<div class="container">
-				<table>
-					<tr><th style="text-align: center;">Name</th><th style="text-align: center;">Team</th><th style="text-align: center;">Overnight Date</th><th style="text-align: center;">Date Submitted</th><th style="text-align: center;">Status</th></tr>
-					@foreach($ons as $ons)
-						<tr><td>{{ $ons->name }}</td><td>{{ $ons->team }}</td><td>{{ date("m/d/Y", strtotime($ons->starting_date)) }} - {{ date("m/d/Y", strtotime($ons->end_date)) }}</td><td>{{ date("m/d/Y", strtotime($ons->created_at)) }}</td><td>{{ $ons->state }}<br><a href="/on_apdetails/{{ $ons->request_id }}">View Details</a></td></tr>
-					@endforeach
-				</table>
-			</div>
-			<br><br>
-		@endif
-		
 		</center>
 		<script>
-			$( document ).ready(function() {
-				var width=$( window ).width();
-				$(".container").width(width-50);
-			});
-			$( window ).resize(function() {
-				var width=$( window ).width();
-				$(".container").width(width-50);
-			});
+		$( document ).ready(function (){
+			var width = $( window ).width();
+			$(".container").width(width*0.9);
+		});
+		$( window ).resize(function (){
+			var width = $( window ).width();
+			$(".container").width(width*0.9);
+		});
 		</script>
 		
     </body>
