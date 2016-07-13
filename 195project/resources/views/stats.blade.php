8bee11dea95b059136afe8db632756cfd4596a57
Jon Aruta <jon.aruta@gmail.com>
2016-07-13 17:58:25 +0800

    weekly and monthly graphs

--- 195project/resources/views/stats.blade.php
+++ 195project/resources/views/stats.blade.php
@@ -12,8 +12,12 @@
 				height:200px;
 			}
 			canvas{
-				max-width: 800px;
+				max-width: 1000px;
 				height: auto !important;
+			}
+			.button{
+				width:100px;
+				margin-left:20px;
 			}
         </style>
     </head>
@@ -27,6 +31,10 @@
 			@else
 				<h3><b>Overall Request Frequency:</b></h3>
 			@endif
+			<button class="button" onclick="init1()">Weekly</button>
+			<button class="button"onclick="init()">Monthly</button>
+			<button class="button">Quarterly</button>
+			<button class="button">Yearly</button>
 			<div id="chart">
 				<canvas id="myChart" height=150></canvas>
 			</div>
@@ -43,7 +51,39 @@
 						labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
 						datasets: [{
 							label: 'Frequency of Requests',
-							data: [12, 19, 3, 5, 2, 3],
+							data: [{{ implode(',',$monthly) }}],
+							backgroundColor: 'rgba(255, 99, 132, 0.2)',
+							borderColor: 'rgba(255,99,132,1)',
+							borderWidth: 1
+						}]
+					},
+					options: {
+						scales: {
+							yAxes: [{
+								ticks: {
+									beginAtZero:true
+								}
+							}]
+						}
+					}
+				});
+			}
+			function init1(){
+				return new Chart(ctx, {
+					responsive: true,
+					maintainAspectRatio: false,
+					type: 'bar',
+					data: {
+						labels: [1
+							<?php
+								for ($i = 2; $i <= 52; $i++) {
+									echo ','.$i;
+								}
+							?>
+						],
+						datasets: [{
+							label: 'Frequency of Requests',
+							data: [{{ implode(',',$weekly) }}],
 							backgroundColor: 'rgba(255, 99, 132, 0.2)',
 							borderColor: 'rgba(255,99,132,1)',
 							borderWidth: 1
